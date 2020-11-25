220290a257bd12475ecfb82b22042b278d4a8bcc
VISHAL GAUR <57058981+gaursuniverse@users.noreply.github.com>
2020-11-25 11:34:30 +0530

    Creating Variables for the assigned id's

--- app/src/main/java/com/canteenapp/paitpooja/Login.java
+++ app/src/main/java/com/canteenapp/paitpooja/Login.java
@@ -1,14 +1,36 @@
 package com.canteenapp.paitpooja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.canteenapp.paitpooja.dashboard.DashboardFirstActivity;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class Login extends AppCompatActivity {
+    TextInputLayout rollNumber, password;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        rollNumber = findViewById(R.id.roll_number);
+        password = findViewById(R.id.password_box);
+        loginBtn = findViewById(R.id.loginBtn);
     }
 }
