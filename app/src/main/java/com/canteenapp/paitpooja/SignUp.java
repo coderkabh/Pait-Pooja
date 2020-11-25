2a294ed7d1b58c9e5ab621601005062d13e96f0d
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-11-25 12:54:02 +0530

    minor bug fix changes to sign up xml file

--- app/src/main/java/com/canteenapp/paitpooja/SignUp.java
+++ app/src/main/java/com/canteenapp/paitpooja/SignUp.java
@@ -28,11 +28,18 @@ public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //This Line will hide the status bar from the screen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
 
+        regName = findViewById(R.id.full_name);
+        regRollNumber = findViewById(R.id.roll_number);
+        regEmail = findViewById(R.id.e_mail);
+        regPhoneNumber = findViewById(R.id.phone_number);
+        regPassword = findViewById(R.id.password_box);
+        regButton = findViewById(R.id.signup_btn);
+        regToLoginBtn = findViewById(R.id.login_btn);
 
+        regselected = findViewById(R.id.spinnerText);
+        reggender = findViewById(R.id.spinnerGender);
 
     }
 }
