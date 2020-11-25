fcb7c2dcb2e94574de5e8e106dd3b192c828bff6
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-11-25 14:55:54 +0530

    added content for RecipeModel.java

--- app/src/main/java/com/canteenapp/paitpooja/Models/RecipeModel.java
+++ app/src/main/java/com/canteenapp/paitpooja/Models/RecipeModel.java
@@ -3,7 +3,41 @@ package com.canteenapp.paitpooja.Models;
 
 public class RecipeModel {
 
+  /*  int pic;    //image is stored in the form of binary digits. so its datatype has been taken as int here.
 
+    String text;
+    //String cartButton;
+
+    public RecipeModel(int pic, String text) {
+        this.pic=pic;
+        this.text=text;
+    }
+
+    public int getPic() {
+        return pic;
+    }
+
+    public void setPic(int pic) {
+        this.pic = pic;
+    }
+
+    //public String getCartButton() {
+    //return cartButton;
+    // }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    *//*
+       public void setCartButton(String cartButton) {
+        this.cartButton = cartButton;
+       }
+       *//*
+*/
 }
 
 
