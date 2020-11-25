34e15beba4a184ab83420505fbe976b718999fab
AnushkaPandey2 <anushka.pandey_cs18@gla.ac.in>
2020-11-25 15:29:00 +0530

    added reference variables

--- app/src/main/java/com/canteenapp/paitpooja/dashboard/DashboardFirstActivity.java
+++ app/src/main/java/com/canteenapp/paitpooja/dashboard/DashboardFirstActivity.java
@@ -1,16 +1,31 @@
 package com.canteenapp.paitpooja.dashboard;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.canteenapp.paitpooja.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class DashboardFirstActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    //Navigation Bar Variables
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout drawerLayout;
+    Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_first);
     }
+
 }
