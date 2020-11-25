2a19d8c0b9dab6477ec26d6065e321aa22a73300
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-11-25 12:47:43 +0530

    added buttons and basic properties to linerar layout of SignUp

--- app/src/main/java/com/canteenapp/paitpooja/SignUp.java
+++ app/src/main/java/com/canteenapp/paitpooja/SignUp.java
@@ -28,5 +28,8 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+
     }
 }
