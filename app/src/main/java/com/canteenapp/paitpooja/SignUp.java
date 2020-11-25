1094f0c6fa653c8323c0fc229fc4cfc76586c6a9
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-11-25 12:38:15 +0530

    Created all reference variables to take references from XML file

--- app/src/main/java/com/canteenapp/paitpooja/SignUp.java
+++ app/src/main/java/com/canteenapp/paitpooja/SignUp.java
@@ -3,8 +3,26 @@ package com.canteenapp.paitpooja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
+
+    TextInputLayout regName, regRollNumber, regEmail, regPhoneNumber, regPassword;
+    Button regButton, regToLoginBtn;
+
+    TextView regselected ;
+    Spinner reggender;
+
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
