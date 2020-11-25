c86ac759fb58f2afaac866a6efcd9803456e9e29
VISHAL GAUR <57058981+gaursuniverse@users.noreply.github.com>
2020-11-25 10:57:25 +0530

    Imported all the required class for splash screen <>

--- app/src/main/java/com/canteenapp/paitpooja/MainActivity.java
+++ app/src/main/java/com/canteenapp/paitpooja/MainActivity.java
@@ -1,8 +1,19 @@
 package com.canteenapp.paitpooja;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
