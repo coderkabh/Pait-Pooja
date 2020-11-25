7e6b1db62a648e8b4575a35f2bb5d9f1ff250521
AnushkaPandey2 <anushka.pandey_cs18@gla.ac.in>
2020-11-25 15:33:44 +0530

    added drawer toolbar

--- app/src/main/java/com/canteenapp/paitpooja/dashboard/DashboardFirstActivity.java
+++ app/src/main/java/com/canteenapp/paitpooja/dashboard/DashboardFirstActivity.java
@@ -2,11 +2,13 @@ package com.canteenapp.paitpooja.dashboard;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.canteenapp.paitpooja.R;
 import com.google.android.material.navigation.NavigationView;
@@ -25,7 +27,25 @@ public class DashboardFirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard_first);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        getTabs();
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.navigation_menu);
+        navigationView.setItemIconTintList(null);
+        drawerLayout = findViewById(R.id.drawer);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
     }
 
 }
