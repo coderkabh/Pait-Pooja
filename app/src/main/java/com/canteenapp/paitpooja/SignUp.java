8904d6b1254d9900f19e1f3457e02be5860734d9
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-11-25 12:51:34 +0530

    added get window to get splash screen from main activity

--- app/src/main/java/com/canteenapp/paitpooja/SignUp.java
+++ app/src/main/java/com/canteenapp/paitpooja/SignUp.java
@@ -3,6 +3,7 @@ package com.canteenapp.paitpooja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -27,6 +28,8 @@ public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //This Line will hide the status bar from the screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
 
 
